b35fe356702ae38c04524b51a8a62d59a60874f0
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-05-18 23:15:56 +0300

    1.3 Группы роутов

--- routes/web.php
+++ routes/web.php
@@ -28,27 +28,30 @@ Route::get('/', function () {
   // return view('welcome');
 });
 
-Route::get('/contacts', function () {
-  return "<h1>All contacts</h1>";
-})->name('contacts.index');
+// Route::prefix('admin')->name('admin.')->group(function () {
+Route::prefix('admin')->group(function () {
+  Route::get('/contacts', function () {
+    return "<h1>All contacts</h1>";
+  })->name('contacts.index');
 
-Route::get('/contacts/create', function () {
-  return "<h1>Add new contact</h1>";
-})->name('contacts.create');
+  Route::get('/contacts/create', function () {
+    return "<h1>Add new contact</h1>";
+  })->name('contacts.create');
 
-// Динамический роут
-Route::get('/contacts/{id}', function ($id) {
-  return "<h1>Contact " . $id . " </h1>";
-  // })->where('id', '[0-9]+');
-})->whereNumber('id')->name('contacts.show');
+  // Динамический роут
+  Route::get('/contacts/{id}', function ($id) {
+    return "<h1>Contact " . $id . " </h1>";
+    // })->where('id', '[0-9]+');
+  })->whereNumber('id')->name('contacts.show');
 
-// Динамический роут с ОПЦИОНАЛЬНЫМ параметром
-Route::get('/companies/{name?}', function ($name = null) {
-  if ($name) {
-    return "<h1>Company " . $name . " </h1>";
-  } else {
-    return "<h1>All Companies</h1>";
-  }
-  // })->where('name', '[a-zA-Z]+');
-  // })->whereAlpha('name');
-})->whereAlphaNumeric('name');
+  // Динамический роут с ОПЦИОНАЛЬНЫМ параметром
+  Route::get('/companies/{name?}', function ($name = null) {
+    if ($name) {
+      return "<h1>Company " . $name . " </h1>";
+    } else {
+      return "<h1>All Companies</h1>";
+    }
+    // })->where('name', '[a-zA-Z]+');
+    // })->whereAlpha('name');
+  })->whereAlphaNumeric('name');
+});
