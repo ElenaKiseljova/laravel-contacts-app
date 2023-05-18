e3ab73d72c6312c81f225561f1e4a9ff1d064cad
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-05-18 20:10:33 +0300

    1.1 Добавлены роуты

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,25 @@ Route::get('/', function () {
   return view('welcome');
 });
 
+Route::get('/contacts', function () {
+  return "<h1>All contacts</h1>";
+});
 
+Route::get('/contacts/create', function () {
+  return "<h1>Add new contact</h1>";
+});
+
+// Динамический роут
+Route::get('/contacts/{id}', function ($id) {
+  return "<h1>Contact " . $id . " </h1>";
+});
+
+// Динамический роут с ОПЦИОНАЛЬНЫМ параметром
+Route::get('/companies/{name?}', function ($name = null) {
+  if ($name) {
+    return "<h1>Company " . $name . " </h1>";
+  } else {
+    return "<h1>All Companies</h1>";
+  }
+});
 
