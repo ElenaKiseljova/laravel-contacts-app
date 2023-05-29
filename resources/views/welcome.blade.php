f1dcb9b4ca6194a37e708cab532be99bd658cdaa
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-05-29 21:30:39 +0300

    2.13 Добавила публичный layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.public')
 
 @section('content')
   <div class="bg-white py-5">
@@ -79,43 +79,4 @@
       </div>
     </div>
   </section>
-
-  <footer class="footer py-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-md">
-          <strong>Contact App</strong>
-          <small class="d-block mb-3">© 2021-2022</small>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Features</h5>
-          <ul class="list-unstyled text-small">
-            <li><a href="#">Email Marketing</a></li>
-            <li><a href="#">Email Template</a></li>
-            <li><a href="#">Email Broadcast</a></li>
-            <li><a href="#">Autoresponder Email</a></li>
-            <li><a href="#">RSS-to-Email</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Resources</h5>
-          <ul class="list-unstyled text-small">
-            <li><a href="#">Landing page Guide</a></li>
-            <li><a href="#">Inbound Marketing Guide</a></li>
-            <li><a href="#">Email Marketing Guide</a></li>
-            <li><a href="#">Helpdesk Guide</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>About</h5>
-          <ul class="list-unstyled text-small">
-            <li><a href="#">Team</a></li>
-            <li><a href="#">Locations</a></li>
-            <li><a href="#">Privacy</a></li>
-            <li><a href="#">Terms</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
 @endsection
