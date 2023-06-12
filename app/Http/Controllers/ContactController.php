ef1ab3a94a260425e069e5b527664c0a3322f51b
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-06-12 15:35:16 +0300

    3.6 Dependency injection

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,17 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\CompanyRepository;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-  public function index()
+  // 1, 2
+  // protected $company;
+
+  // 3
+  public function __construct(protected CompanyRepository $company)
+  // 2
+  // public function __construct(CompanyRepository $company)
+  // 1
+  // public function __construct()
   {
-    $companies = [
-      1 => ['name' => 'Company One', 'contacts' => 3],
-      2 => ['name' => 'Company Two', 'contacts' => 1],
-      3 => ['name' => 'Company Three', 'contacts' => 4]
-    ];
+    // 2
+    $this->company = $company;
+    // 1
+    // $this->company = new CompanyRepository();
+  }
+
+  // 2
+  public function index(CompanyRepository $company, Request $request)
+  // 1
+  // public function index()
+  {
+    // dd($request->sort_by);
+
+    // $companies = [
+    //   1 => ['name' => 'Company One', 'contacts' => 3],
+    //   2 => ['name' => 'Company Two', 'contacts' => 1],
+    //   3 => ['name' => 'Company Three', 'contacts' => 4]
+    // ];
+
+    // 2
+    $companies = $company->pluck();
+    // 1
+    // $companies = $this->company->pluck();
 
     $contacts = $this->getContacts();
 
