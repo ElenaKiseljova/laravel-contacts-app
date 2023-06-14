fb928c68635b1c5f6e4352d3b3f8fed6ff454cb3
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-06-14 20:35:32 +0300

    7.2 Применение фабрик

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Company;
 use App\Models\Contact;
-use Faker\Factory as Faker;
 
 class ContactSeeder extends Seeder
 {
@@ -15,28 +12,6 @@ class ContactSeeder extends Seeder
    */
   public function run(): void
   {
-    $companies = Company::all();
-    $faker = Faker::create();
-
-    $contacts = [];
-
-    foreach ($companies as $company) {
-      foreach (range(1, mt_rand(5, 15)) as $index) {
-        $contact = [
-          'first_name' => $faker->firstName(),
-          'last_name' => $faker->lastName(),
-          'phone' => $faker->phoneNumber(),
-          'email' => $faker->email(),
-          'address' => $faker->address(),
-          'company_id' => $company->id,
-          'created_at' => now(),
-          'updated_at' => now()
-        ];
-
-        $contacts[] = $contact;
-      }
-    }
-
-    Contact::insert($contacts);
+    Contact::factory()->count(100)->create();
   }
 }
