4680d7cf64b4b49dfd96810feef759be893143c0
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-06-15 10:21:39 +0300

    8.1 HTML формы. Фильтр компаний

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,58 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\CompanyRepository;
-use Illuminate\Http\Request;
 use App\Models\Contact;
-use Illuminate\Pagination\LengthAwarePaginator;
 
 class ContactController extends Controller
 {
-  // 1, 2
-  // protected $company;
-
-  // 3
   public function __construct(protected CompanyRepository $company)
-  // 2
-  // public function __construct(CompanyRepository $company)
-  // 1
-  // public function __construct()
   {
-    // 2
     $this->company = $company;
-    // 1
-    // $this->company = new CompanyRepository();
   }
 
-  // 2
-  public function index(CompanyRepository $company, Request $request)
-  // 1
-  // public function index()
+  public function index()
   {
-    // dd($request->sort_by);
+    $companies = $this->company->pluck();
 
-    // $companies = [
-    //   1 => ['name' => 'Company One', 'contacts' => 3],
-    //   2 => ['name' => 'Company Two', 'contacts' => 1],
-    //   3 => ['name' => 'Company Three', 'contacts' => 4]
-    // ];
-
-    // 2
-    $companies = $company->pluck();
-    // 1
-    // $companies = $this->company->pluck();
-
-    // $contacts = $this->getContacts();
-    // $contacts = Contact::latest()->paginate(10);
-    $contactsCollection = Contact::latest()->get();
-    $perPage = 10;
-    $currentPage = request()->query('page', 1);
-    $items = $contactsCollection->slice(($currentPage * $perPage) - $perPage, $perPage);
-    $total = $contactsCollection->count();
-
-    $contacts = new LengthAwarePaginator($items, $total, $perPage, $currentPage, [
-      'path' => request()->url(),
-      'query' => request()->query()
-    ]);
+    $contacts = Contact::latest()->where(function ($query) {
+      if ($company_id = request()->query('company_id')) {
+        $query->where('company_id', $company_id);
+      }
+    })->paginate(10);
 
     return view('contacts.index', compact('contacts', 'companies'));
   }
@@ -66,9 +32,6 @@ class ContactController extends Controller
 
   public function show($id)
   {
-    // $contact = Contact::find($id);
-    // abort_if(empty($contact), 404);
-
     $contact = Contact::findOrFail($id);
 
     return view('contacts.show')->with('contact', $contact);
