454bb7da0ede1e9b50365dba7e6ba171b43c6ed3
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-08-02 18:46:00 +0300

    16.1 Debugging queries: DB::listen()

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
   public function boot(): void
   {
     Paginator::useBootstrapFour();
+
+    // Запись лога, если в .env APP_DEBUG = true
+    if (config('app.debug')) {
+      DB::listen(fn ($query) => Log::info($query->sql, $query->bindings, $query->time));
+    }
   }
 }
